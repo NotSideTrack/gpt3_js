5ea1eb7741723a4430b13802ab1df8c6c9a09694
SideTrack <bryantan2009@hotmail.com>
2023-06-19 20:30:19 +0800

    On branch master Initial commit Changes to be committed: 	new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt     new file:   src/App.css 	new file:   src/App.js 	new file:   src/assets/ai.png 	new file:   src/assets/atlassian.png 	new file:   src/assets/blog01.png 	new file:   src/assets/blog02.png 	new file:   src/assets/blog03.png 	new file:   src/assets/blog04.png 	new file:   src/assets/blog05.png 	new file:   src/assets/dropbox.png 	new file:   src/assets/google.png 	new file:   src/assets/logo.png 	new file:   src/assets/logo.svg 	new file:   src/assets/people.png 	new file:   src/assets/possibility.png 	new file:   src/assets/shopify.png 	new file:   src/assets/slack.png 	new file:   src/components/article/Article.jsx 	new file:   src/components/article/article.css 	new file:   src/components/brand/Brand.jsx 	new file:   src/components/brand/brand.css 	new file:   src/components/brand/import.js 	new file:   src/components/cta/CTA.jsx 	new file:   src/components/cta/cta.css 	new file:   src/components/feature/Feature.jsx 	new file:   src/components/feature/feature.css 	new file:   src/components/index.js 	new file:   src/components/navbar/Navbar.jsx 	new file:   src/components/navbar/navbar.css 	new file:   src/containers/blog/Blog.jsx 	new file:   src/containers/blog/blog.css 	new file:   src/containers/features/Features.jsx 	new file:   src/containers/features/features.css 	new file:   src/containers/footer/Footer.jsx 	new file:   src/containers/footer/footer.css 	new file:   src/containers/header/Header.jsx 	new file:   src/containers/header/header.css 	new file:   src/containers/index.js 	new file:   src/containers/possibility/Possibility.jsx 	new file:   src/containers/possibility/possibility.css 	new file:   src/containers/whatGPT3/WhatGPT3.jsx 	new file:   src/containers/whatGPT3/whatGPT3.css 	new file:   src/index.css 	new file:   src/index.js

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import "./navbar.css";
+import logo from "../../assets/logo.svg";
+
+const Menu = () => {};
+
+function Navbar() {
+  //first one is variable, 2nd function that allows us to change variable
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={35}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={35}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <p>
+                <a href="#home">Home</a>
+              </p>
+              <p>
+                <a href="#wgpt3">What is GPT3?</a>
+              </p>
+              <p>
+                <a href="#possibility">Open AI</a>
+              </p>
+              <p>
+                <a href="#features">Case Studies</a>
+              </p>
+              <p>
+                <a href="#blog">Library</a>
+              </p>
+            </div>
+            <div className="gpt3__navbar-menu_container-links-sign">
+              <p>Sign in</p>
+              <button type="button">Sign up</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+
+// {toggleMenu ? (
+//   <RiCloseLine
+//     color="#FFF"
+//     size={27}
+//     onClick={() => setToggleMenu(false)}
+//   />
+// ) : (
+//   <RiMenu3Line
+//     color="#FFF"
+//     size={27}
+//     onClick={() => setToggleMenu(true)}
+//   />
+// )}
+// {toggleMenu && (
+//   <div className="gpt3__navbar-menu_container scale-up-center">
+//     <div className="gpt3__navbar-menu_container-links">
+//       <p>
+//         <a href="#home">Home</a>
+//       </p>
+//       <p>
+//         <a href="#wgpt3">What is GPT3?</a>
+//       </p>
+//       <p>
+//         <a href="#possibility">Open AI</a>
+//       </p>
+//       <p>
+//         <a href="#features">Case Studies</a>
+//       </p>
+//       <p>
+//         <a href="#blog">Library</a>
+//       </p>
+//     </div>
+//   </div>
+// )}
